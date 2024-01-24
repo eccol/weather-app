f0d4d82e62858acb58d7c978d5f49565a161b582
Eric <126840543+eccol@users.noreply.github.com>
2024-01-24 11:52:49 -0500

    Create DisplayController

---
+++ src/DisplayController.js
@@ -0,0 +1,27 @@
+export class DisplayController {
+  constructor() {
+    this.inputField = document.getElementById('city');
+    this.currentTemp = document.querySelector('.temperature');
+    this.feelsLike = document.querySelector('.feels-like');
+    this.conditions = document.querySelector('.condition');
+    this.wind = document.querySelector('.wind');
+    this.forecast = [
+      document.querySelector('.day1'),
+      document.querySelector('.day2'),
+      document.querySelector('.day3'),
+    ];
+  }
+
+  updateAll(data) {
+    const location = `${data.cityName}, ${data.countryName}`;
+    this.inputField.value = location;
+
+    this.currentTemp.innerText = data.currentTemp;
+    this.feelsLike.innerText = data.feelsLike;
+    this.conditions.innerText = data.condition;
+    this.wind.innerText = `${data.windSpeed} ${data.windDir}`;
+    for (let i = 0; i < this.forecast.length; i++) {
+      this.forecast[i].innerText = data.forecastAvg[i];
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import getWeather from './getWeather.js';
+import { DisplayController } from './DisplayController.js';
 
-function updateTemp(data) {
-  const cityField = document.querySelector('.location');
-  cityField.innerText = `${data.cityName}, ${data.countryName}`;
+const displayController = new DisplayController();
 
-  const tempField = document.querySelector('.temperature');
-  tempField.innerText = `${data.currentTemp} degrees Fahrenheit`;
-}
+// function updateTemp(data) {
+//   const cityField = document.querySelector('.location');
+//   cityField.innerText = `${data.cityName}, ${data.countryName}`;
+
+//   const tempField = document.querySelector('.temperature');
+//   tempField.innerText = `${data.currentTemp} degrees Fahrenheit`;
+// }
 
 function displayError(err) {
   const tempField = document.querySelector('.temperature');
@@ -17,5 +20,6 @@ document.querySelector('button').addEventListener('click', () => {
   const city = document.getElementById('city').value;
   const data = getWeather(city);
 
-  data.then(updateTemp, displayError);
+  // data.then(updateTemp, displayError);
+  data.then((d) => displayController.updateAll(d), displayError);
 });

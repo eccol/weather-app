7faefabdeae5c54bb109a7bde2dd6d9c45a33cb3
Eric <126840543+eccol@users.noreply.github.com>
2024-01-24 11:53:38 -0500

    Clean comments

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import { DisplayController } from './DisplayController.js';
 
 const displayController = new DisplayController();
 
-// function updateTemp(data) {
-//   const cityField = document.querySelector('.location');
-//   cityField.innerText = `${data.cityName}, ${data.countryName}`;
-
-//   const tempField = document.querySelector('.temperature');
-//   tempField.innerText = `${data.currentTemp} degrees Fahrenheit`;
-// }
-
 function displayError(err) {
   const tempField = document.querySelector('.temperature');
   tempField.innerText = `${err}`;
@@ -20,6 +12,5 @@ document.querySelector('button').addEventListener('click', () => {
   const city = document.getElementById('city').value;
   const data = getWeather(city);
 
-  // data.then(updateTemp, displayError);
   data.then((d) => displayController.updateAll(d), displayError);
 });

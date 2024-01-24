0232dae55a7f292914482f0e1734792f18352998
Eric <126840543+eccol@users.noreply.github.com>
2024-01-23 19:34:34 -0500

    Create fetch function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 const API_KEY = process.env.API_KEY;
+
+async function getWeather(city) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
+
+  const data = await fetch(url);
+  const json = await data.json();
+  const currentTemp = await json.current.temp_f;
+  console.log('current temp is', currentTemp);
+}
+
+getWeather('Orlando');

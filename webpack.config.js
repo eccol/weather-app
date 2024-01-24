ee2d68380553f3f935e0e1c929f95a23c0c96378
Eric <126840543+eccol@users.noreply.github.com>
2024-01-23 19:28:58 -0500

    Set up API key environment variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./src",
+    static: './src',
   },
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
+    new Dotenv(),
   ],
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
 };
